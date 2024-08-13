4f0b4a08a8ad2fbdea097030b0e7478753429c67
Carlos Prieto Barrón <carlosprietobarron@outlook.com>
2024-08-13 16:14:37 -0600

    delete things

--- WinFormsApp1/CustomControl1.cs
+++ WinFormsApp1/CustomControl1.cs
@@ -16,31 +16,27 @@ namespace WinFormsApp1
         {
             base.OnPaint(e);
 
-            // Ejemplo de pintura personalizada
-            // Puedes modificar este código para personalizar tu control
-
-            // Pintar un rectángulo rojo en el control
+            
             using (Brush brush = new SolidBrush(Color.Red))
             {
                 e.Graphics.FillRectangle(brush, this.ClientRectangle);
             }
 
-            // Pintar texto en el control
+           
             using (Font font = new Font("Arial", 14))
             {
                 e.Graphics.DrawString("Custom Control", font, Brushes.White, new PointF(10, 10));
             }
         }
 
-        // Puedes agregar propiedades y métodos personalizados aquí
+        
         public string CustomText { get; set; } = "Default Text";
 
         protected override void OnMouseClick(MouseEventArgs e)
         {
             base.OnMouseClick(e);
-            // Ejemplo: Cambiar el color del control al hacer clic
             this.BackColor = this.BackColor == Color.Red ? Color.Blue : Color.Red;
-            this.Invalidate(); // Forzar redibujado
+            this.Invalidate(); 
         }
     }
 }
